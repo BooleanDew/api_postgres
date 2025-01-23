2d0837c9e6067e25cc418427042ab3cf99a719ce
BooleanDew <booleandew@gmail.com>
2025-01-23 17:02:49 -0300

    ok

--- conexion.js
+++ conexion.js
@@ -1,13 +1,13 @@
 import pgPromise from 'pg-promise';
-
-
-const pgp = pgPromise({}); 
-const connectionString = process.env.DATABASE_URL; 
+import { config } from 'dotenv'
+config()
+const pgp = pgPromise({});
+const connectionString = (process.env.DEVELOPER) ? process.env.DATABASE_URL_DEV:process.env.DATABASE_URL
 const db = pgp(connectionString);
 
 
 export async function datos() {
-  return await db.query("select * from usuarios")
+  return await db.query("select * from chats")
     .then((data) => data)
     .catch((error) => error);
 }
